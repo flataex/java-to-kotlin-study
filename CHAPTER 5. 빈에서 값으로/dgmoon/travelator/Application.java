a9a3a1b2059c179fb9a96c5198a11f48620c3331
Donggeon Moon <83414846+DonggeonMoon@users.noreply.github.com>
2023-01-23 19:18:41 +0900

    Create Application.java

---
+++ CHAPTER 5. 빈에서 값으로/dgmoon/travelator/Application.java
@@ -0,0 +1,17 @@
+package travelator.ch5;
+
+public class Application {
+    private final UserPreferences preferences;
+
+    public Application(UserPreferences preferences) {
+        this.preferences = preferences;
+    }
+
+    public void showWelcome() {
+        new WelcomeView(preferences).show();
+    }
+
+    public void editPreferences() {
+        new PreferencesView(preferences).show();
+    }
+}
